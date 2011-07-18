cfd7c50bcb451271681bf9c93a5d7f1f8ecbd12c
Steven Jones <steven.jones@computerminds.co.uk>
2011-07-18 12:37:22 +0100

    Get server provisioning working.

--- helpers/cm_libcloud.py
+++ helpers/cm_libcloud.py
@@ -1,7 +1,7 @@
 from libcloud.compute.types import Provider
 from libcloud.compute.providers import get_driver
 from libcloud.compute.deployment import MultiStepDeployment, ScriptDeployment, SSHKeyDeployment
-import os, sys, random, string, ConfigParser
+import os, sys, random, string, ConfigParser, time
 
 import libcloud.security
 libcloud.security.VERIFY_SSL_CERT = True
@@ -47,3 +47,26 @@ def get_images():
 def get_sizes():
     conn = get_connection()
     return conn.list_sizes()
+    
+def create_server(selected_image, selected_size, node_name=None):
+    if node_name is None:
+        node_name = 'CM Mercury ' + str(time.time())
+    images = get_images();
+    sizes = get_sizes();
+    
+    # We'll use the distro and size from the config ini
+    preferred_image = [image for image in images if selected_image in image.name]
+    assert len(preferred_image) == 1, "We found more than one image for %s, will be assuming the first one" % selected_image
+
+    preferred_size = [size for size in sizes if selected_size in size.name]
+    
+    dispatch = [
+        SSHKeyDeployment(open(os.path.expanduser("~/.ssh/id_rsa.pub")).read()),
+    ]
+    msd = MultiStepDeployment(dispatch)
+    
+    conn = get_connection()
+    node = conn.deploy_node(name=node_name,image=preferred_image[0], size=preferred_size[0], deploy=msd)
+    
+    return node
+
