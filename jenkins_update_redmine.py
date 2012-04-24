0b43448a8a89bf32cf3698a33ee223956e9c2367
Steven Jones <steven.jones@computerminds.co.uk>
2012-04-24 13:18:03 +0100

    Beginnings of an update Redmine script.

---
+++ jenkins_update_redmine.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+
+import update_redmine
+import os, sys
+
+if __name__ == "__main__":
+    args = [
+        'dummy',
+        "--db_password=" + os.environ['DB_PASSWORD'] + "",
+        "--redmine_host=" + os.environ['REDMINE_HOST'] + "",
+        "--release_tag=" + os.environ['RELEASE_TAG'] + "",
+    ]
+    sys.exit(update_redmine.main(args))
+
+# --size=$CM_SIZE --image=$CM_IMAGE --enable_apache=$CM_ENABLE_APACHE --enable_mysql=$CM_ENABLE_MYSQL --enable_jenkins=$CM_ENABLE_JENKINS --enable_memcache=$CM_ENABLE_MEMCACHE --enable_tomcat=$CM_ENABLE_TOMCAT
---
+++ update_redmine.py
@@ -0,0 +1,86 @@
+#! /usr/bin/env python
+
+"""Update Redmine script
+
+Updates the Computerminds instance of Redmine
+
+Valid options:
+  --db_password - The database password to use.
+  --redmine_host - The Redmine server to connect to.
+  --release_tag - The redmine version to checkout.
+"""
+
+import sys, getopt
+import fabric.api as fabric
+
+class Usage(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+    try:
+        try:
+            opts, args = getopt.getopt(argv[1:], "h", ["help", "db_password=", "redmine_host=", "release_tag="])
+        except getopt.error, msg:
+             raise Usage(msg)
+        # process options
+        db_password = None
+        redmine_host = None
+        release_tag = None
+
+        for o, a in opts:
+            if o in ("-h", "--help"):
+                print __doc__
+                return 0;
+            if o in ("--db_password"):
+                db_password = a
+            if o in ("--redmine_host"):
+                redmine_host = a
+            if o in ("--download_url"):
+                download_url = a
+
+        if db_password is None:
+            raise Usage, "the --db_password option must be specified"
+        if redmine_host is None:
+            raise Usage, "the --redmine_host option must be specified"
+        if release_tag is None:
+            raise Usage, "the --release_tag option must be specified"
+
+        update_redmine(db_password, redmine_host, release_tag)
+
+        return 0
+
+    except Usage, err:
+        print >>sys.stderr, err.msg
+        print >>sys.stderr, "for help use --help"
+        return 2
+
+def logger(message):
+    print ("===> %s" % message)
+
+def update_redmine(db_password = None, redmine_host = None, release_tag = None):
+
+    # Connect to the aegir host.
+    fabric.env.host_string = redmine_host
+    fabric.env.user = 'root'
+
+    logger('Stopping apache server')
+    fabric.run("/etc/init.d/apache stop", pty=True)
+
+    logger('Backing up MySQL')
+    fabric.run("mysql -u root -p%s redmine | gzip > /tmp/redmine_`date +%y_%m_%d`.gz" % (), pty=True)
+
+    logger('Downloading Redmine')
+    fabric.run("cd /var/www/support && git clone https://github.com/redmine/redmine.git redmine-%s && cd redmine-%s && git checkout %s" % (release_tag, release_tag, release_tag), pty=True)
+
+    logger('Restarting thin')
+    fabric.run("/etc/init.d/thin restart", pty=True)
+
+    logger('Starting apache server')
+    fabric.run("/etc/init.d/apache start", pty=True)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
