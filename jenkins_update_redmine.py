416ef3414574a167d18bac033237778bd98fa865
Steven Jones <steven.jones@computerminds.co.uk>
2016-04-22 15:32:40 +0100

    Add a parameter for invalidating sessions.

--- jenkins_update_redmine.py
+++ jenkins_update_redmine.py
@@ -10,6 +10,8 @@ if __name__ == "__main__":
         "--redmine_host=" + os.environ['REDMINE_HOST'] + "",
         "--release_tag=" + os.environ['RELEASE_TAG'] + "",
     ]
+    if os.environ['INVALIDATE_SESSIONS'] == "yes":
+        args.append("--invalidate_sessions")
     sys.exit(update_redmine.main(args))
 
 # --size=$CM_SIZE --image=$CM_IMAGE --enable_apache=$CM_ENABLE_APACHE --enable_mysql=$CM_ENABLE_MYSQL --enable_jenkins=$CM_ENABLE_JENKINS --enable_memcache=$CM_ENABLE_MEMCACHE --enable_tomcat=$CM_ENABLE_TOMCAT
